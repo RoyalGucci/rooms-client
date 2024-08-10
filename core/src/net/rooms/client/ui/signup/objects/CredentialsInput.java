cc49178eee1fb93f7abbc32fb77ef62f9f4fa1df
bubbleship <84926115+bubbleship@users.noreply.github.com>
2024-08-10 18:18:59 +0300

    feat(signup): add keyboard control to signup screen
    Pressing 'enter' is equivalent to pressing 'signup'.
    Pressing 'up' moves the keyboard focus to the field above the current one.
    Pressing 'down' moves the keyboard focus to the field below the current one.

--- core/src/net/rooms/client/ui/signup/objects/CredentialsInput.java
+++ core/src/net/rooms/client/ui/signup/objects/CredentialsInput.java
@@ -1,7 +1,10 @@
 package net.rooms.client.ui.signup.objects;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -9,6 +12,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import net.rooms.client.ui.signup.SignupScreen;
+
+import java.util.List;
 
 public class CredentialsInput extends Table {
 
@@ -34,6 +39,12 @@ public class CredentialsInput extends Table {
 
 		confirmPasswordField = new TextField("", skin);
 		confirmPasswordField.setMessageText("Confirm password");
+
+		CredentialsInputListener credentialsInputListener = new CredentialsInputListener();
+		nicknameField.addListener(credentialsInputListener);
+		usernameField.addListener(credentialsInputListener);
+		passwordField.addListener(credentialsInputListener);
+		confirmPasswordField.addListener(credentialsInputListener);
 
 		TextButton signup = new TextButton("Signup", skin);
 		TextButton login = new TextButton("Already signed up? click me", skin);
@@ -93,5 +104,26 @@ public class CredentialsInput extends Table {
 			return false;
 		}
 		return true;
+	}
+
+	private final class CredentialsInputListener extends InputListener {
+
+		// Cannot be used before the below text fields are initialized
+		List<Actor> fields = List.of(new Actor[]{nicknameField, usernameField, passwordField, confirmPasswordField});
+
+		@Override
+		public boolean keyDown(InputEvent event, int keycode) {
+			if (keycode == Input.Keys.ENTER || keycode == Input.Keys.NUMPAD_ENTER) {
+				signup();
+				return true;
+			}
+
+			int currentFieldIndex = fields.indexOf(event.getListenerActor());
+			if (keycode == Input.Keys.UP && currentFieldIndex > 0)
+				getStage().setKeyboardFocus(fields.get(currentFieldIndex - 1));
+			else if (keycode == Input.Keys.DOWN && currentFieldIndex < fields.size() - 1)
+				getStage().setKeyboardFocus(fields.get(currentFieldIndex + 1));
+			return true;
+		}
 	}
 }
