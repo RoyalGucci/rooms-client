f96c4533639fd99c72132dee80f3a649e5ce9e2a
RoyalGucci <96540857+RoyalGucci@users.noreply.github.com>
2024-08-27 20:06:08 +0300

    added a screen for searching with joining feature

---
+++ core/src/net/rooms/client/ui/search/objects/SearchChatInfo.java
@@ -0,0 +1,60 @@
+package net.rooms.client.ui.search.objects;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import net.rooms.client.connection.objects.PublicRoom;
+import net.rooms.client.ui.search.SearchScreen;
+
+public class SearchChatInfo extends Table {
+
+	private final Skin skin;
+	private final Table chatContainer;
+	private final SearchScreen screen;
+
+	public SearchChatInfo(SearchScreen screen) {
+		this.screen = screen;
+		skin = new Skin(Gdx.files.internal("skin-composer\\skin\\skin-composer-ui.json"));
+		setBackground(skin.newDrawable("white", 0.8f, 0.8f, 0.8f, 1));
+
+		chatContainer = new Table();
+		chatContainer.top();
+		ScrollPane scrollPane = new ScrollPane(chatContainer, skin);
+		scrollPane.setFadeScrollBars(false);
+		add(scrollPane).expand().fill().pad(10);
+		row();
+	}
+
+	public void setInteractive(boolean interactive) {
+		Touchable touchable = interactive ? Touchable.enabled : Touchable.disabled;
+		chatContainer.setTouchable(touchable);
+	}
+
+	public void setRoom() {
+		PublicRoom current = screen.getRoom();
+		TextButton joinButton = new TextButton("Join", skin);
+		TextField password = new TextField("", skin);
+		password.setMessageText("password");
+		chatContainer.add(new Label(current.title(), skin)).pad(20);
+		chatContainer.row();
+		chatContainer.add(new Label(current.description(), skin)).pad(20);
+		chatContainer.row();
+		chatContainer.add(password).pad(20);
+		chatContainer.row();
+		chatContainer.add(joinButton).pad(20);
+
+		joinButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				screen.getClient().getApiRequests().joinRoom(current.roomID(), password.getText());
+			}
+		});
+	}
+
+	public void resetContent() {
+		chatContainer.clearChildren();
+		screen.getStage().setKeyboardFocus(chatContainer);
+	}
+}
---
+++ core/src/net/rooms/client/ui/search/objects/SearchNavPanel.java
@@ -0,0 +1,53 @@
+package net.rooms.client.ui.search.objects;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import net.rooms.client.ui.search.SearchScreen;
+
+public class SearchNavPanel extends Table {
+	public SearchNavPanel(SearchScreen screen) {
+		Skin skin = new Skin(Gdx.files.internal("skin-composer\\skin\\skin-composer-ui.json"));
+		top().left();
+		setBackground(skin.newDrawable("white", 0.3f, 0.3f, 0.3f, 1));
+
+		Texture texture = new Texture(Gdx.files.internal("door.png"));
+		ImageButton logout = new ImageButton(new TextureRegionDrawable(texture));
+		Texture texture2 = new Texture(Gdx.files.internal("search.png"));
+		ImageButton search = new ImageButton(new TextureRegionDrawable(texture2));
+		Texture texture3 = new Texture(Gdx.files.internal("dashboard.png"));
+		ImageButton dashboard = new ImageButton(new TextureRegionDrawable(texture3));
+		logout.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				screen.getClient().getScreenManager().login();
+				screen.getClient().getApiRequests().logout();
+				screen.getClient().getRepository().clear();
+			}
+		});
+		search.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				//TODO: IMPLEMENT NEW SCREEN
+			}
+		});
+		dashboard.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				screen.getClient().getScreenManager().dashboard();
+			}
+		});
+		add(logout).pad(5).left();
+		row();
+		add(search).padTop(20).left();
+		row();
+		add(dashboard).padTop(20).padLeft(5).left();
+		row();
+	}
+}
+
