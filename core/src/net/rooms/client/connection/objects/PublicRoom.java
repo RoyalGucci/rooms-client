a65314c3cf55d528e3fdf4942f8ac00f7961e1d2
bubbleship <84926115+bubbleship@users.noreply.github.com>
2024-08-28 18:39:47 +0300

    docs: add missing documentation to PublicRoom.java

--- core/src/net/rooms/client/connection/objects/PublicRoom.java
+++ core/src/net/rooms/client/connection/objects/PublicRoom.java
@@ -2,6 +2,17 @@ package net.rooms.client.connection.objects;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents a single room the same way it would appear on search queries.
+ * The fields below are publicly visible to all users of the platform.
+ *
+ * @param roomID       A unique identifier for the room.
+ * @param title        The title/name of the room.
+ * @param hasPassword  Indicates whether the room has a password.
+ * @param owner        The username of the user that created the room.
+ * @param creationDate The date and time when the room was created.
+ * @param description  A string of text that describes the room.
+ */
 public record PublicRoom(
 		long roomID,
 		String title,
