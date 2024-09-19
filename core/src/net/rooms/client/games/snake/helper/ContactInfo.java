c881c2415b0a6d9b46ae237b076b569a26f62e17
bubbleship <84926115+bubbleship@users.noreply.github.com>
2024-09-19 19:42:04 +0300

    refactor: remove unused file

--- core/src/net/rooms/client/games/snake/helper/ContactInfo.java
+++
@@ -1,9 +0,0 @@
-package net.rooms.client.games.snake.helper;
-
-import net.rooms.client.games.pong.helper.ContactType;
-
-public record ContactInfo(
-		ContactType contactType,
-		String username
-) {
-}
